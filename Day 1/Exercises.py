0d348a865ef761ee93b6e7a314d14075acf187a6
Krystian <krystian.tomaszewski3@gmail.com>
2018-12-09 16:14:08 +0100

    End day

---
+++ Day 1/Exercises.py
@@ -0,0 +1,9 @@
+a = "aaaaabbbbcccc"
+
+
+for letter in a:
+    if letter in dictionary:
+        dictionary[letter] +=1
+    else
+        dictionary[letter] = 1
+print (dictionary)
--- Day 1/First step.py
+++ Day 1/First step.py
@@ -296,3 +296,99 @@ e = "I"
 f = " "
 
 print (b +f + c + f + a + f + e + f + d)
+
+eval("2+2")
+
+print (2 * (45 / 5))
+print ((45 / 2) * 8)
+
+
+a = 1
+print (str(a))
+
+print (str(1) + "a")
+print (2 + int("1"))
+
+a = 1000
+V1 = 3/100
+V2 = 7/100
+V3 = 23/100
+
+print (round(a / (V1 + 1),2))
+print (round(a / (V2 + 1),2))
+print (round(a / (V3 + 1),2))
+
+
+print
+chleb = 1.99
+Mleko = 2.5
+Cukierki = 12.99
+
+print (round((2 * chleb) + (0.5 * Mleko) + (0.3 * Cukierki),2))
+
+a = 0o11
+print (a)
+
+a = 0o27
+print (a)
+
+
+a = """
+przypisany do zmiennej, długiego ciągu znaków
+"""
+print (a)
+
+a = 7
+print (bool(a))
+
+imię = "Adam"
+nazwisko = "Kowalski"
+wiek = "35"
+pensja = "6000 brutto"
+stanowisko = "inżynier procesu"
+
+print (10*(imię +" "+ nazwisko +" "+"(" + "wiek"+" "+ wiek + ")" +" "
+    + "pracuje na stanowisku" +" " + stanowisko +" "+"(" + "pensja:"+" "+pensja + ")"))
+
+K = 500
+M1 = 700
+M2 = M1 * 1.5
+M3 = M2 * 1.5
+M4 = M3 * 1.5
+M5 = M4 * 1.5
+M6 = M5 * 1.5
+print (M1 -K)
+print (M2 -K)
+print (M3 -K)
+print (M4 -K)
+print (M5 -K)
+print (M6 -K)
+
+V = 23/100
+WN = 5500
+H = 168
+WB = round(5500 * (V + 1),2)
+SH = round(WB / H,2)
+print (WB)
+print (SH)
+
+a = bool(0)
+b = bool(0)
+c = bool(1)
+B1 = (not a) and (not b) and (not c)
+B2 = (not a) and (not b) and c
+B3 = (not a) and b and (not c)
+B4 = a and (not b) and (not c)
+WK = B1 or B2 or B3 or B4
+print (WK)
+print ("\"")
+"""
+imie = input("wczyta napis")
+print (30* (imie + "\n"))
+
+W = input("Wysokość")
+P = input("Pole Podstawy")
+X = 1/2
+PO = X * W * P
+print (PO)
+"""
